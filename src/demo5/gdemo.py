7bb101db758399b2c397a46e4f5b3b7f2ee5cd04
asampley <sampley@ualberta.ca>
2017-03-09 17:02:50 -0700

    Stuff

--- src/demo5/gdemo.py
+++ src/demo5/gdemo.py
@@ -31,7 +31,10 @@ criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 objp = np.zeros((6*8,3), np.float32)
 objp[:,:2] = np.mgrid[0:8,0:6].T.reshape(-1,2)
 
-axis = np.float32([[3,0,0], [0,3,0], [0,0,-3]]).reshape(-1,3)
+objp = objp * 0.026
+print(objp)
+
+axis = np.float32([[0.1,0,0], [0,0.1,0], [0,0,-0.1]]).reshape(-1,3)
 
 #print("running")
 #cv2.imshow("test",image)
@@ -56,14 +59,14 @@ class Drawer:
 
     gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
     image = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
-#    ret, corners = cv2.findChessboardCorners(gray, (8,6), None)
-    ret, corners = cv2.findChessboardCorners(gray, (8,6), None, cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_NORMALIZE_IMAGE)
+    ret, corners = cv2.findChessboardCorners(gray, (8,6), None)
+#    ret, corners = cv2.findChessboardCorners(gray, (8,6), None, cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_NORMALIZE_IMAGE)
 
-    cv2.drawChessboardCorners(image, (8,6), corners, ret)
+#    cv2.drawChessboardCorners(image, (8,6), corners, ret)
 
     if ret == True:
         #corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
-        cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+        #cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
 
         # Find the rotation and translation vectors.
         #rvecs, tvecs, inliers = cv2.solvePnPRansac(objp, corners2, mtx, dist)
@@ -88,13 +91,14 @@ class Drawer:
 #        print("2")
 #        print(imgpts[2])
         print("target" + str(tvecs))
+        print("rotation" + str(rvecs))
 
         # if we are far from the checkerboard, move forward and turn
-        if tvecs[2] > 20:
+        if tvecs[2] > 0.3:
           self.twist.linear.x = 0.05
-          if tvecs[0] > 1:
+          if tvecs[0] > 0.1:
             self.twist.angular.z = -0.1
-          elif tvecs[0] < -1:
+          elif tvecs[0] < -0.1:
             self.twist.angular.z = 0.1
           else:
             self.twist.angular.z = 0
