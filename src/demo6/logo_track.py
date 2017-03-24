dd442f4bec9ebb609426449b148a78a0850ab9f4
asampley <sampley@ualberta.ca>
2017-03-24 09:42:06 -0600

    Played around with parameters, but did not get much farther along

--- src/demo6/logo_track.py
+++ src/demo6/logo_track.py
@@ -23,12 +23,15 @@ cv2.namedWindow('img', 1)
 cv2.waitKey(1)
 bridge = cv_bridge.CvBridge()
 
-img1 = cv2.imread('uofa.png',0)          # queryImage
+img1 = cv2.imread('uofa.png', 0)          # queryImage
 print(img1)
 cv2.waitKey(1000)
+orb = cv2.ORB()
+kp1 = orb.detect(img1,None)
+kp1, des1 = orb.compute(img1,kp1)
 
 def image_callback(msg):
-  global img1
+  global img1, orb, kp1, des1, mtx, dist, bridge, axis
 
   found=True
   tvec=np.zeros((1,3))
@@ -39,19 +42,16 @@ def image_callback(msg):
   img2 = img2.squeeze(axis=2)
   print(img1.shape)
   print(img2.shape)
-  orb = cv2.ORB()
 
   # find the keypoints and descriptors with SIFT
-  kp1 = orb.detect(img1,None)
-  kp1, des1 = orb.compute(img1,kp1)
   kp2 = orb.detect(img2,None)
   kp2, des2 = orb.compute(img2,kp2)
 
-  #FLANN_INDEX_KDTREE = np.array([0], dtype=np.float32)
+  #FLANN_INDEX_KDTREE = 0
   #index_params = {"algorithm" : FLANN_INDEX_KDTREE, "trees" : 5}
   #search_params = {"checks" : 50}
-  #
-  #flann = cv2.FlannBasedMatcher(index_params, search_params)
+  
+  #flann = FlannBasedMatcher(index_params, search_params)
   #
   #matches = flann.knnMatch(des1,des2,k=2)
 
@@ -63,6 +63,18 @@ def image_callback(msg):
   for m,n in matches:
     if m.distance < 0.7*n.distance:
         good.append(m)
+
+#  # use threshold distance and cross check instead of ratio test
+#  bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+#  matches = bf.match(des1, des2)
+#  
+#  distances = [m.distance for m in matches]
+#  thres_dist = (sum(distances) / len(distances)) * 0.7
+#  good = [m for m in matches if m.distance < thres_dist]
+
+#  # sort and trim to at most 20 good keypoints
+#  good.sort(key=lambda match: match.distance)
+#  good = good[:min(len(good)-1, 20)]
 
   if len(good)>MIN_MATCH_COUNT:
     src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
@@ -77,18 +89,21 @@ def image_callback(msg):
 
     cv2.polylines(img2,[np.int32(dst)],True,255,3)
 
+    maskedMatches = [good[i] for i in range(len(good)) if matchesMask == 1]
+
+    img3 = draw_matches(img1,kp1,img2,kp2,maskedMatches,color=255)
   else:
     #print "Not enough matches are found - %d/%d" % (len(good),MIN_MATCH_COUNT)
     matchesMask = None
+    img3 = draw_matches(img1,kp1,img2,kp2,good,color=127)
 
-  draw_params = dict(matchColor = (0,255,0), # draw matches in green color
-                   singlePointColor = None,
-                   matchesMask = matchesMask, # draw only inliers
-                   flags = 2)
+  #draw_params = dict(matchColor = (0,255,0), # draw matches in green color
+  #                 singlePointColor = None,
+  #                 matchesMask = matchesMask, # draw only inliers
+  #                 flags = 2)
 
   #print(img1)
   #print(img2)
-  img3 = draw_matches(img1,kp1,img2,kp2,good)
   #plt.imshow(img3, 'gray'),plt.show()
   
   if found:
